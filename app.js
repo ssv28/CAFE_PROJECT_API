49452b7a8a963db2764d9d7ab44ada7edbbed7f3
Your Name <you@example.com>
2024-12-03 11:10:49 +0530

    Add

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
+require('dotenv').config()
 
 const mongoose = require('mongoose');
 
@@ -25,6 +27,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
